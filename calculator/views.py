567f3636b1d9caebb47f89de52b2a65e093f9ab2
“Aleksandr <ross1990@mail.ru>
2023-04-25 23:38:59 +0300

    before review

--- calculator/views.py
+++ calculator/views.py
@@ -15,16 +15,27 @@ DATA = {
         'колбаса, ломтик': 1,
         'сыр, ломтик': 1,
         'помидор, ломтик': 1,
-    },
-    # можете добавить свои рецепты ;)
+    }
 }
 
-# Напишите ваш обработчик. Используйте DATA как источник данных
-# Результат - render(request, 'calculator/index.html', context)
-# В качестве контекста должен быть передан словарь с рецептом:
-# context = {
-#   'recipe': {
-#     'ингредиент1': количество1,
-#     'ингредиент2': количество2,
-#   }
-# }
+def dishes_calculator(request, dish):
+    servings = request.GET.get('servings', None)
+    recipe = None
+    text = None
+    if dish:
+        if dish in DATA:
+            if servings:
+                servings = int(servings)
+                new_recipe = {}
+                for key, value in DATA[dish].items():
+                    new_recipe[key] = value * servings
+                recipe = new_recipe
+            else:
+                recipe = DATA[dish]
+        else:
+            text = 'Not found'
+    context = {
+        'recipe': recipe,
+        'text': text,
+    }
+    return render(request, 'calculator/index.html', context)
--- recipes/urls.py
+++ recipes/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
+
+from calculator.views import dishes_calculator
 
 urlpatterns = [
-    # здесь зарегистрируйте вашу view-функцию
+    path('admin/', admin.site.urls),
+    path('<dish>/', dishes_calculator, name='recipe'),
 ]
